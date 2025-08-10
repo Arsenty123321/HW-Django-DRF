6df5311fd24b871ab0b1a5a77c860f75de10b74d
Arsenty Dafintsev <aaapython24@gmail.com>
2025-08-10 04:28:27 +0300

    Add periodic task celery-beat block_inactive_users()

--- config/settings.py
+++ config/settings.py
@@ -150,3 +150,12 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
 CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
+
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+CELERY_BEAT_SCHEDULE = {
+    "block_inactive_users": {
+        "task": "lms.tasks.block_inactive_users",
+        "schedule": timedelta(days=1),  # run at every day
+    },
+}
